aac34b4ff8036fd54cbdb4ede2db78897e6b8469
Jakub Krol <jkrol@sointeractive.pl>
2017-10-16 12:28:17 +0200

    add conf templates

---
+++ tests/test_default.py
@@ -0,0 +1,47 @@
+from testinfra.utils.ansible_runner import AnsibleRunner
+
+testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
+
+
+def test_directories(File):
+    present = [
+        "/etc/td-agent-bit"
+    ]
+    if present:
+        for directory in present:
+            d = File(directory)
+            assert d.is_directory
+            assert d.exists
+
+
+def test_files(File):
+    present = [
+        "/etc/td-agent/td-agent-bit.conf"
+    ]
+    if present:
+        for file in present:
+            f = File(file)
+            assert f.exists
+            assert f.is_file
+
+
+def test_service(Service):
+    present = [
+        "td-agent-bit"
+    ]
+    if present:
+        for service in present:
+            s = Service(service)
+            assert s.is_enabled
+            assert s.is_running
+
+
+def test_packages(Package):
+    present = [
+        "td-agent-bit"
+    ]
+    if present:
+        for package in present:
+            p = Package(package)
+            assert p.is_installed
+
